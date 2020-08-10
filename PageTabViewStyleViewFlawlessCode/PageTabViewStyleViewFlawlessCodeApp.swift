8317c046fe249ee3c1d8a0556c7052de95d59f3a
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-08-10 11:25:46 +0300

    Initial Commit

---
+++ PageTabViewStyleViewFlawlessCode/PageTabViewStyleViewFlawlessCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PageTabViewStyleViewFlawlessCodeApp.swift
+//  PageTabViewStyleViewFlawlessCode
+//
+//  Created by ramil on 10.08.2020.
+//
+
+import SwiftUI
+
+@main
+struct PageTabViewStyleViewFlawlessCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
